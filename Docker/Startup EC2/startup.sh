ef415bd97d4c48af76e4506511988164767d794f
Benjamin Davis DC, SAA-C02, AZ-900 <112713133+benjamindavisdc@users.noreply.github.com>
2023-06-14 22:53:03 -0500

    Delete startup.sh
    Updated with bootstrap.sh

--- Docker/Startup EC2/startup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Build and run Apache container
-docker build -t my-apache-server /path/to/apache-dockerfile
-docker run -d -p 80:80 my-apache-server
-
-# Build and run Jenkins container
-docker build -t my-jenkins-server /path/to/jenkins-dockerfile
-docker run -d -p 8080:8080 my-jenkins-server
